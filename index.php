82069f0ece6f9c75d57ba6aef8b6544b743915a8
anasch132 <45896633+anasch132@users.noreply.github.com>
2020-09-17 20:28:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ foreach($collects as $collect){
 
     		$images = shopify_call($token, $shop, "/admin/api/2020-07/products/".$products['product']['id']."/images.json", array(), 'GET');
 		$images = json_decode($images['response'], JSON_PRETTY_PRINT);
-		$item_default_image = $images['images'][0]['src'];
+		$item_default_image = $images['images'][1]['src'];
 
 		echo '<img src="'.$item_default_image.'" style="width: 200px; height: 230px;"/>';
     } 
