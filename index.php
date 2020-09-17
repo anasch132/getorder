845ac5c217f48ea832fd9c86aae34fab4854d76d
anasch132 <45896633+anasch132@users.noreply.github.com>
2020-09-17 20:16:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,8 @@ foreach($collects as $collect){
     foreach($collect as $key => $value){ 
     		$products = shopify_call($token, $shop, "/admin/api/2020-07/products/".$value['collection_id'].".json", array(), 'GET');
 		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
-
+		echo $products['product']['id'];
+	    exit();
     		$images = shopify_call($token, $shop, "/admin/api/2020-07/products/".$products['product']['id']."/images.json", array(), 'GET');
 		$images = json_decode($images['response'], JSON_PRETTY_PRINT);
 		$item_default_image = $images['images'][0]['src'];
