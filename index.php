0505dc261da997344a6062907f508dcce246541a
anasch132 <anassech132@gmail.com>
2020-09-18 20:42:24 +0100

    woodecor

--- index.php
+++ index.php
@@ -5,10 +5,17 @@ session_start();
 require __DIR__ . '/vendor/autoload.php';
 
 $config = array(
-    'ShopUrl' => 'firstone132.myshopify.com',
-    'AccessToken' => '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',
+    'ShopUrl' => '611381deaed2660dfdc8645ce603dc0c',
+    'ApiKey' => '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',
+    'SharedSecret' => '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',
 );
 
+$scopes = 'read_products,write_products,read_script_tags,write_script_tags, read_orders, write_orders';
+//This is also valid
+//$scopes = array('read_products','write_products','read_script_tags', 'write_script_tags'); 
+$redirectUrl = 'https://getorder.herokuapp.com/index.php';
+
+\PHPShopify\AuthHelper::createAuthRequest($scopes, $redirectUrl, null, null, true);
 
 $shopify = new PHPShopify\ShopifySDK($config);
 
@@ -60,6 +67,15 @@ $shopify->Order->post($order);
 echo "order done!";
 exit();
 }
+// $list = array(
+//     'status' => 'open',
+//     'created_at_min' => '2016-06-25T16:15:47-04:00',
+//     'fields' => 'email, id, phone'
+// );
+// $orders = $shopify->Order->get($list);
+
+
+// print_r($orders);
 
 
 ?>
--- install.php
+++ install.php
@@ -2,14 +2,16 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "7ebf9c0f47534e46963ff61f89347a88";
-$scopes = 'read_products,write_products,read_script_tags,write_script_tags, read_orders, write_orders';
 
+$api_key = "611381deaed2660dfdc8645ce603dc0c";
+$scopes = 'read_products,write_products,read_script_tags,write_script_tags, read_orders, write_orders';
 $redirect_uri = "https://getorder.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
-$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+$install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 
 // Redirect
 header("Location: " . $install_url);
 die();
+
+?>
