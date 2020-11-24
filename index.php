e294b230af736915aaf6a0684fed8a073d1072bd
anasch132 <anassech132@gmail.com>
2020-11-24 18:39:25 +0100

    zinatic

--- index.php
+++ index.php
@@ -21,8 +21,6 @@ $shopify = new PHPShopify\ShopifySDK($config);
 
 $products = $shopify->Product->get();
 
-if (isset($_POST['submit']))
-{
 
     $order = array (
       "order" => [
@@ -67,7 +65,6 @@ $shopify->Order->post($order);
 
 
 echo "order done!";
-}
 function filtered($var)
 {
     // returns whether the input integer is even
@@ -88,31 +85,3 @@ $product = array_filter($product, "filtered");
 // print_r($orders);
 // // exit();
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>buy now</title>
-</head>
-<body>
-
-<form method="post">
-<input type="text" name="first_name" placeholder="first_name"> </input>
-<input type="text" name="last_name" placeholder="last_name"> </input>
-<input type="text" name="address1" placeholder="address1"> </input>
-<input type="text" name="email" placeholder="email"> </input>
-<input type="text" name="phone" placeholder="phone"> </input>
-<input type="text" name="city" placeholder="city"> </input>
-<input type="text" name="province" placeholder="province"> </input>
-<input type="text" name="zip" placeholder="zip"> </input>
-<input type="text" name="country" placeholder="country"> </input>
-<input type="submit" name="submit" value="order now"></input>
-
-
-</form>
-    
-</body>
-</html>
