2f79e62f50b8769fa9007c603a33bbfd78f27d1f
hello <root@10.12.4.12>
2020-11-25 11:22:13 +0000

    added

--- index.php
+++ index.php
@@ -23,8 +23,12 @@ $products = $shopify->Product->get();
 
 if (1)
 {
+  $phone = $_GET['phone'];
+  $name = $_GET['name'];
+  $address = $_GET['address'];
+
     $order = array (
-      "phone"=> $_GET['phone'],
+      "phone"=> $phone,
       "fulfillment_status" => "unfulfilled",
         "send_receipt" => true,
         "send_fulfillment_receipt" => true,
@@ -35,25 +39,25 @@ if (1)
         ]
       ],
       "customer" => [
-        "first_name" => $_GET['name'],
+        "first_name" => $name,
         "last_name" => "blank",
-        "emal" => $_GET['email']
+        "emal" => "blank"
       ],
       "billing_address" => [
-        "first_name" => $_GET['name'],
+        "first_name" => $name,
         "last_name" => "blank",
-        "address1" => $_GET['address'],
-        "phone" => $_GET['phone'],
+        "address1" => $address,
+        "phone" => $phone,
         "city" => "blank",
         "province" => "blank",
         "country" => "blank",
         "zip" => "blank"
       ],
       "shipping_address"=> [
-        "first_name"=> $_GET['name'],
+        "first_name"=> $name,
         "last_name"=> "blank",
-        "address1"=> $_GET['address'],
-        "phone"=> $_GET['phone'],
+        "address1"=> $address,
+        "phone"=> $phone,
         "city"=> "blank",
         "province"=> "blank",
         "country"=> "blank",
@@ -67,63 +71,5 @@ $shopify->Order->post($order);
 
 echo "order done!";
 }
-// function filtered($var)
-// {
-//     // returns whether the input integer is even
-//     return $var["handle"] == "its-a-test-1";
-// }
-
-// $product = $shopify->Product()->get();
-
-// $product = array_filter($product, "filtered");
-// // print_r($product);
-
-
-// // $list = array(
-// //     'status' => 'any',
-// // );
-// // $orders = $shopify->Order->get($list);
-
-// // print_r($orders);
-// // // exit();
-// foreach ($product as $oneorder)
-// {
-//    foreach($oneorder as $elem)
-//    {
-//     print($elem);
-//     echo "<br>";
-//    }
-//     // print($oneorder[0]);
-//    echo "<br><br>--------------------------------------------<br>";
-    
-// }
 
 ?>
-
-<!-- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>buy now</title>
-</head>
-<body>
-
-<form method="get">
-<input type="text" name="first_name" placeholder="first_name"> </input>
-<input type="text" name="last_name" placeholder="last_name"> </input>
-<input type="text" name="address1" placeholder="address1"> </input>
-<input type="text" name="email" placeholder="email"> </input>
-<input type="text" name="phone" placeholder="phone"> </input>
-<input type="text" name="city" placeholder="city"> </input>
-<input type="text" name="province" placeholder="province"> </input>
-<input type="text" name="zip" placeholder="zip"> </input>
-<input type="text" name="country" placeholder="country"> </input>
-<input type="submit" name="submit" value="order now"></input>
-
-
-</form>
-    
-</body>
-</html> -->
