d6cb0b7534c7dee0454c7a5b90e03aabbc68737c
hello <root@10.12.4.12>
2020-11-25 11:01:48 +0000

    added

--- index.php
+++ index.php
@@ -21,11 +21,10 @@ $shopify = new PHPShopify\ShopifySDK($config);
 
 $products = $shopify->Product->get();
 
-if (isset($_POST['submit']))
+if (1)
 {
-
     $order = array (
-      "phone"=> $_POST['phone'],
+      "phone"=> $_GET['phone'],
       "fulfillment_status" => "unfulfilled",
         "send_receipt" => true,
         "send_fulfillment_receipt" => true,
@@ -36,29 +35,29 @@ if (isset($_POST['submit']))
         ]
       ],
       "customer" => [
-        "first_name" => $_POST['first_name'],
-        "last_name" => $_POST['last_name'],
-        "emal" => $_POST['email']
+        "first_name" => $_GET['name'],
+        "last_name" => "blank",
+        "emal" => $_GET['email']
       ],
       "billing_address" => [
-        "first_name" => $_POST['first_name'],
-        "last_name" => $_POST['last_name'],
-        "address1" => $_POST['address1'],
-        "phone" => $_POST['phone'],
-        "city" => $_POST['city'],
-        "province" => $_POST['province'],
-        "country" => $_POST['country'],
-        "zip" => $_POST['zip']
+        "first_name" => $_GET['name'],
+        "last_name" => "blank",
+        "address1" => $_GET['address'],
+        "phone" => $_GET['phone'],
+        "city" => "blank",
+        "province" => "blank",
+        "country" => "blank",
+        "zip" => "blank"
       ],
       "shipping_address"=> [
-        "first_name"=> $_POST['first_name'],
-        "last_name"=> $_POST['last_name'],
-        "address1"=> $_POST['address1'],
-        "phone"=> $_POST['phone'],
-        "city"=> $_POST['city'],
-        "province"=> $_POST['province'],
-        "country"=> $_POST['country'],
-        "zip"=> $_POST['zip']
+        "first_name"=> $_GET['name'],
+        "last_name"=> "blank",
+        "address1"=> $_GET['address'],
+        "phone"=> $_GET['phone'],
+        "city"=> "blank",
+        "province"=> "blank",
+        "country"=> "blank",
+        "zip"=> "blank"
       ],
       "financial_status" => "pending",
 );
@@ -68,40 +67,40 @@ $shopify->Order->post($order);
 
 echo "order done!";
 }
-function filtered($var)
-{
-    // returns whether the input integer is even
-    return $var["handle"] == "its-a-test-1";
-}
+// function filtered($var)
+// {
+//     // returns whether the input integer is even
+//     return $var["handle"] == "its-a-test-1";
+// }
 
-$product = $shopify->Product()->get();
+// $product = $shopify->Product()->get();
 
-$product = array_filter($product, "filtered");
-// print_r($product);
+// $product = array_filter($product, "filtered");
+// // print_r($product);
 
 
-// $list = array(
-//     'status' => 'any',
-// );
-// $orders = $shopify->Order->get($list);
+// // $list = array(
+// //     'status' => 'any',
+// // );
+// // $orders = $shopify->Order->get($list);
 
-// print_r($orders);
-// // exit();
-foreach ($product as $oneorder)
-{
-   foreach($oneorder as $elem)
-   {
-    print($elem);
-    echo "<br>";
-   }
-    // print($oneorder[0]);
-   echo "<br><br>--------------------------------------------<br>";
+// // print_r($orders);
+// // // exit();
+// foreach ($product as $oneorder)
+// {
+//    foreach($oneorder as $elem)
+//    {
+//     print($elem);
+//     echo "<br>";
+//    }
+//     // print($oneorder[0]);
+//    echo "<br><br>--------------------------------------------<br>";
     
-}
+// }
 
 ?>
 
-
+<!-- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -111,7 +110,7 @@ foreach ($product as $oneorder)
 </head>
 <body>
 
-<form method="post">
+<form method="get">
 <input type="text" name="first_name" placeholder="first_name"> </input>
 <input type="text" name="last_name" placeholder="last_name"> </input>
 <input type="text" name="address1" placeholder="address1"> </input>
@@ -127,4 +126,4 @@ foreach ($product as $oneorder)
 </form>
     
 </body>
-</html>
+</html> -->
